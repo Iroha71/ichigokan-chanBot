6b354dc5192ddfca0d9dc858a4a6c690386e669d
Iroha71 <y.koba.h1071@outlook.jp>
2019-04-29 20:57:56 +0900

    add: google place api追加

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const server=require("express")();
 const line=require("@line/bot-sdk");
+const request=require("request");
 
+const gglplaceApi=`https://maps.googleapis.com/maps/api/place/nearbysearch/json?`;
+const gglMapApi="https://maps.googleapis.com/maps/api/js";
+const key="AIzaSyD1kAwMRJfaAHuEtlL3qGRDlVxde0jiyKM";
 const line_config={
     channelAccessToken: process.env.LINE_ACCESS_TOKEN,
     channelSecret: process.env.LINE_CHANNEL_SECRET
@@ -14,7 +18,14 @@ server.post('/bot/webhook',line.middleware(line_config),(req,res,next)=>{
     let evt_prc=[];
     req.body.events.forEach((event) => {
         if(event.type=="message" && event.message.type=="text"){
-            if(event.message.text=="こんにちは" ||event.message.text=="にゃにゃ"){
+            if(event.message.text=="飲食店"){
+                request.get({
+                    uri: gglplaceApi+`key=${key}&location=33.5337369,130.3802676&radius=1500&language=ja&type=restaurant`,
+                    headers: {'Content-type': 'application/json'},
+                    json: true
+                },function(err,req,data){
+                    console.log(data);
+                })
                 const resp={
                     "type": "template",
                     "altText": "代替テキスト",
